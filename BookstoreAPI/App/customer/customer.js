a578d6f6c4a5351bc9ca4f16e91571553806b449
Anand Maran <Anand Maran@DESKTOP-TRVQ9FI>
2017-04-25 22:50:41 +1000

    Made modifications to KO html file

---
+++ BookstoreAPI/App/customer/customer.js
@@ -0,0 +1,22 @@
+﻿var vm = function(names){
+var self = this; //use this in KO for managing scope hassle.
+
+self._names = ko.observableArray(names);
+self._newval = ko.observable(null);
+
+self._addnewval = function(){
+	console.log(self._newval());
+	if(self._newval() != null){
+
+			console.log('inside function');
+			self._names.push(self._newval());
+			self._newval(null);
+
+	}
+};
+
+	
+
+};
+
+ko.applyBindings(new vm(['Anand','Maran','Priya']));
