1e1e4d7643f5a067963f4a945e344d177615499a
kolabrate <Ana1982(>
2017-04-25 14:10:59 +1000

    Added - view - knockout twitter

---
+++ BookstoreAPI/Migrations/201704250026031_CreateTablesinBookstoreDB.cs
@@ -0,0 +1,16 @@
+namespace BookstoreAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTablesinBookstoreDB : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
